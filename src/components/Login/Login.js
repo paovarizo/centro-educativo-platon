1c1df8d3394931fe27c09d3d3944485b7df5e387
Paola Vazquez <paovats@gmail.com>
2023-05-30 00:02:42 -0600

    add navbar

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,11 @@
+import AppBarSystem from "../AppBarSystem/AppBarSystem";
 import { LoginRender } from "./LoginRender"
 const Login =()=>{
     return (
+        <>
+        <AppBarSystem/>
         <LoginRender/>
+        </>
     )
 }
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,11 @@
+import AppBarSystem from "../AppBarSystem/AppBarSystem";
 import { RegisterRender } from "./RegisterRender"
 const Register =()=>{
     return (
+        <>
+        <AppBarSystem/>
         <RegisterRender/>
+        </>
     )
 }
 export default Register;

92d2e95c2ce943e2fbbd7de36a78691c2088de90
Paola Vazquez <paovats@gmail.com>
2023-06-04 11:00:16 -0600

    add input validations

--- src/components/Login/LoginRender.js
+++ src/components/Login/LoginRender.js
@@ -1,7 +1,38 @@
 import './Login.scss'
 import Grid from '@mui/material/Grid';
 import Animation from '../../assets/login-animation.gif'
+import React, { useEffect, useState } from 'react';
+import Alert from '@mui/material/Alert';
+import { Snackbar } from '@mui/material';
+
 export const LoginRender = ()=> {
+    const [mail, setMail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeMail = (event)=>{
+        setMail(event?.taget?.value);
+    }
+    const handlePassword = (event)=>{
+        setPassword(event?.target?.value);
+    }
+    const handleSubmitLogin =()=>{
+        if(mail === '' || password === ''){
+            handleClick()
+        }
+    }
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpen(false);
+    };
     return(
         <>
         <div className='Grid-container'>
@@ -17,10 +48,10 @@ export const LoginRender = ()=> {
                         <br></br>
                         <p>Por favor llena los siguientes campos :</p>
                         <label>Correo electronico</label>
-                        <input className='input-mail'/>
+                        <input className='input-mail' onChange={handleChangeMail} value={mail}/>
                         <label>Contraseña</label>
-                        <input className='input-password'/>
-                        <button className='button-enter'>INGRESAR</button>
+                        <input className='input-password' onChange={handlePassword} value={password}/>
+                        <button className='button-enter' onClick={handleSubmitLogin}>INGRESAR</button>
                         <div className='Container-forgot-password'>
                             <a href='/recover'>Olvide mi contraseña</a>
                         </div>
@@ -28,6 +59,11 @@ export const LoginRender = ()=> {
                 </Grid>
             </Grid>
         </div>
+        <Snackbar open={open} onClose={handleClose} autoHideDuration={2000}>
+            <Alert onClose={handleClose} severity="error">
+            Por favor llena todos los campos
+            </Alert>
+        </Snackbar>
         </>
     )
 }
--- src/components/Register/RegisterRender.js
+++ src/components/Register/RegisterRender.js
@@ -2,12 +2,56 @@ import './Register.scss'
 import Grid from '@mui/material/Grid';
 import Animation from '../../assets/register-animation.gif'
 import { useEffect, useState } from 'react';
+import Alert from '@mui/material/Alert';
+import { Snackbar } from '@mui/material';
+
 export const RegisterRender = ()=> {
     const [inputDate, setInputDate] = useState('');
     const [displayMenor, setDisplayMenor] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [parents, setParents] = useState('');
+
     const handleChangeDate= (event)=> {
         setInputDate(event.target.value)
     }
+    const handleChangeMail = (event)=>{
+        setEmail(event?.taget?.value);
+    }
+    const handlePassword = (event)=>{
+        setPassword(event?.target?.value);
+    }
+    const handleChangeName = (event)=>{
+        setName(event?.taget?.value);
+    }
+    const handleParents = (event)=>{
+        setParents(event?.target?.value);
+    }
+    const handleSubmitLogin =()=>{
+        if(displayMenor){
+            if(email === '' || password === '' || parents=== '' || inputDate==='' || name==='' ){
+                handleClick()
+            }
+        }else{
+            if(email === '' || password === ''  || inputDate==='' || name==='' ){
+                handleClick()
+            }   
+        }
+    }
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpen(false);
+    };
     useEffect(()=>{
         const new_date = new Date(inputDate)
         const actual_date = new Date()
@@ -24,21 +68,21 @@ export const RegisterRender = ()=> {
                         <br></br>
                         <p>Por favor llena los siguientes campos :</p>
                         <label>Nombre completo</label>
-                        <input className='input-name'/>
+                        <input className='input-name' onChange={handleChangeName} value={name}/>
                         <label>Correo electronico</label>
-                        <input className='input-mail'/>
+                        <input className='input-mail' onChange={handleChangeMail} value={email}/>
                         <label>Fecha de nacimiento</label>
                         <input className='input-date-birth' type='date' value={inputDate} onChange={handleChangeDate}/>
                         {
                             displayMenor &&                         
                             <>
                             <label>Nombre completo del padre o tutor</label>
-                            <input className='input-name-parents'/>
+                            <input className='input-name-parents' onChange={handleParents} value={parents}/>
                             </>
                         }
                         <label>Contraseña</label>
-                        <input className='input-password' type='password'/>
-                        <button className='button-enter'>CONFIRMAR REGISTRO</button>
+                        <input className='input-password' type='password' onChange={handlePassword} value={password}/>
+                        <button className='button-enter' onClick={handleSubmitLogin}>CONFIRMAR REGISTRO</button>
                     </div>
                 </Grid>
                 <Grid item xs={6} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
@@ -48,6 +92,11 @@ export const RegisterRender = ()=> {
                 </Grid>
             </Grid>
         </div>
+        <Snackbar open={open} onClose={handleClose} autoHideDuration={2000}>
+            <Alert onClose={handleClose} severity="error">
+            Por favor llena todos los campos
+            </Alert>
+        </Snackbar>
         </>
     )
 }
